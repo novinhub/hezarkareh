712ecbcb3ae4ee4ee55b5f2024a05307a9800dc1
novinhub <48179603+novinhub@users.noreply.github.com>
2019-04-15 19:40:38 +0430

    resume

--- application/views/candidates/resume.php
+++ application/views/candidates/resume.php
@@ -1,42 +1,40 @@
                <form action="#" class="job-post-form rtl">
                   <div class="basic-info-input">
-                    <h4><i data-feather="plus-circle"></i>Add Resume</h4>
+               
                     <div id="full-name" class="form-group row">
-                      <label class="col-md-3 col-form-label">Full Name</label>
+                      <label class="col-md-3 col-form-label">نام و نام خانوادگی:</label>
                       <div class="col-md-9">
-                        <input type="text" class="form-control" placeholder="Your Name">
+                        <input type="text" class="form-control" placeholder="نام و نام خانوادگی خود را وارد کنید">
                       </div>
                     </div>
                     <div id="information" class="row">
-                      <label class="col-md-3 col-form-label">Information</label>
+                      <label class="col-md-3 col-form-label">درباره من:</label>
                       <div class="col-md-9">
                         <div class="row">
                           <div class="col-md-6">
                             <div class="form-group">
                               <select class="form-control">
-                                <option>Select Category</option>
-                                <option>Accounting / Finance</option>
-                                <option>Health Care</option>
-                                <option>Garments / Textile</option>
-                                <option>Telecommunication</option>
+                                <option>وضعیت تاهل : </option>
+                                <option>مجرد</option>
+                                <option>متاهل</option>
                               </select>
                               <i class="fa fa-caret-down"></i>
                             </div>
                           </div>
                           <div class="col-md-6">
                             <div class="form-group">
-                              <input type="text" class="form-control" placeholder="Your Location">
+                              <input type="text" class="form-control" placeholder="محل سکونت خود را وارد کنید ">
                             </div>
                           </div>
                           <div class="col-md-6">
                             <div class="form-group">
                               <select class="form-control">
-                                <option>Job Type</option>
-                                <option>Part Time</option>
-                                <option>Full Time</option>
-                                <option>Temperory</option>
-                                <option>Permanent</option>
-                                <option>Freelance</option>
+                                <option>وضعیت خدمت</option>
+                                <option>معافیت تحصیلی</option>
+                                <option>معاف</option>
+                                <option>اتمام خدمت</option>
+                                <option>مشمول</option>
+                                <option>نرفته</option>
                               </select>
                               <i class="fa fa-caret-down"></i>
                             </div>
@@ -44,142 +42,125 @@
                           <div class="col-md-6">
                             <div class="form-group">
                               <select class="form-control">
-                                <option>Experience (Optional)</option>
-                                <option>Less than 1 Year</option>
-                                <option>2 Year</option>
-                                <option>3 Year</option>
-                                <option>4 Year</option>
-                                <option>Over 5 Year</option>
+                                <option>وضعیت شغلی</option>
+                                <option>شاغل</option>
+                                <option>بیکار</option>
+                                <option>شاغل و جویای کار</option>
                               </select>
                               <i class="fa fa-caret-down"></i>
                             </div>
                           </div>
                           <div class="col-md-6">
                             <div class="form-group">
-                              <input type="text" class="form-control" placeholder="Salary Range">
+                              <input type="text" class="form-control" placeholder="شماره همراه">
                             </div>
                           </div>
+                          <div class="col-md-6">
+                            <div class="form-group">
+                              <input type="text" class="form-control" placeholder="شماره تلفن">
+                            </div>
+                          </div>
+                    
                           <div class="col-md-6">
                             <div class="form-group">
                               <select class="form-control">
-                                <option>Qualification</option>
-                                <option>Matriculation</option>
-                                <option>Intermidiate</option>
-                                <option>Gradute</option>
+                                <option>جنسیت</option>
+                                <option>مرد</option>
+                                <option>زن</option>
                               </select>
                               <i class="fa fa-caret-down"></i>
                             </div>
                           </div>
                           <div class="col-md-6">
                             <div class="form-group">
-                              <select class="form-control">
-                                <option>Gender</option>
-                                <option>Male</option>
-                                <option>Female</option>
-                              </select>
-                              <i class="fa fa-caret-down"></i>
-                            </div>
-                          </div>
-                          <div class="col-md-6">
-                            <div class="form-group">
-                              <input type="date" class="form-control">
-                            </div>
-                          </div>
-                          <div class="col-md-12">
-                            <div class="form-group datepicker">
-                              <input type="text" class="form-control" placeholder="Your Skill">
+                              <input type="date" class="form-control" placeholder="تاریخ تولد" >
                             </div>
                           </div>
                         </div>
                       </div>
                     </div>
-                    <div id="about" class="row">
-                      <label class="col-md-3 col-form-label">About You</label>
-                      <div class="col-md-9">
-                        <textarea class="tinymce-editor-1" placeholder="Description text here"></textarea>
-                      </div>
-                    </div>
+                  
                     <div id="education" class="row">
-                      <label class="col-md-3 col-form-label">Education</label>
+                      <label class="col-md-3 col-form-label">تحصیلات:</label>
                       <div class="col-md-9">
                         <div class="form-group">
-                          <input type="text" class="form-control" placeholder="Designation">
+                          <input type="text" class="form-control" placeholder="رشته تحصیلی">
                         </div>
                       </div>
                     </div>
                     <div class="row">
-                      <label class="col-md-3 col-form-label">EDUCATION 01</label>
+                      <label class="col-md-3 col-form-label"></label>
                       <div class="col-md-9">
                         <div class="form-group">
-                          <input type="text" class="form-control" placeholder="Institute">
+                          <input type="text" class="form-control" placeholder="گرایش">
                         </div>
                         <div class="form-group">
-                          <input type="text" class="form-control" placeholder="Period">
+                          <input type="text" class="form-control" placeholder="دانشگاه">
                         </div>
                         <div class="form-group">
-                          <textarea class="form-control" placeholder="Description (Optional)"></textarea>
+                          <textarea class="form-control" placeholder="توضیخات"></textarea>
                         </div>
-                        <a href="#" class="add-new-field">+ Add Education</a>
+                        <!-- <a href="#" class="add-new-field">+ Add Education</a> -->
                       </div>
                     </div>
                     <div id="experience" class="row">
-                      <label class="col-md-3 col-form-label">Work Experience</label>
+                      <label class="col-md-3 col-form-label">پیشینه شغلی :</label>
                       <div class="col-md-9">
                         <div class="form-group">
-                          <input type="text" class="form-control" placeholder="Designation">
+                          <input type="text" class="form-control" placeholder="نام شرکت">
                         </div>
                       </div>
                     </div>
                     <div class="row">
-                      <label class="col-md-3 col-form-label">Experience 01</label>
+                      <label class="col-md-3 col-form-label"></label>
                       <div class="col-md-9">
                         <div class="form-group">
-                          <input type="text" class="form-control" placeholder="Company Name">
+                          <input type="text" class="form-control" placeholder="سطح ارشدیت">
                         </div>
                         <div class="form-group">
-                          <input type="text" class="form-control" placeholder="Period">
+                          <input type="text" class="form-control" placeholder="گروه شغلی">
                         </div>
                         <div class="form-group">
-                          <textarea class="form-control" placeholder="Description (Optional)"></textarea>
+                          <textarea class="form-control" placeholder="توضیحات"></textarea>
                         </div>
-                        <a href="#" class="add-new-field">+ Add Experience</a>
+                        <!-- <a href="#" class="add-new-field">+ Add Experience</a> -->
                       </div>
                     </div>
                     <div id="skill" class="row">
-                      <label class="col-md-3 col-form-label">Professional Skill</label>
+                      <label class="col-md-3 col-form-label">مهارت ها</label>
                       <div class="col-md-9">
                         <div class="form-group">
-                          <input type="text" class="form-control" placeholder="Designation">
+                          <input type="text" class="form-control" placeholder="دانش و مهارت">
                         </div>
                       </div>
                     </div>
                     <div class="row">
-                      <label class="col-md-3 col-form-label">SKILL 01</label>
+                      <label class="col-md-3 col-form-label"></label>
                       <div class="col-md-9">
                         <div class="form-group">
-                          <input type="text" class="form-control" placeholder="Title">
+                          <input type="text" class="form-control" placeholder="میزان تسلط">
                         </div>
                         <div class="form-group">
-                          <input type="number" class="form-control" placeholder="value (Percen)">
+                          <input type="number" class="form-control" placeholder="توضیحات">
                         </div>
-                        <a href="#" class="add-new-field">+ Add Experience</a>
+                        <!-- <a href="#" class="add-new-field">+ Add Experience</a> -->
                       </div>
                     </div>
-                    <div id="qualification" class="row">
+                    <!-- <div id="qualification" class="row">
                       <label class="col-md-3 col-form-label">Special Qualification</label>
                       <div class="col-md-9">
                         <textarea id="tiny" class="tinymce-editor-2" placeholder="Description text here"></textarea>
                       </div>
-                    </div>
-                    <div id="portfolio" class="row">
+                    </div> -->
+                    <!-- <div id="portfolio" class="row">
                       <label class="col-md-3 col-form-label">Portfolio</label>
                       <div class="col-md-9">
                         <div class="form-group">
                           <input type="text" class="form-control" placeholder="Title">
                         </div>
                       </div>
-                    </div>
-                    <div class="row">
+                    </div> -->
+                    <!-- <div class="row">
                       <label class="col-md-3 col-form-label">PORTFOLIO 01</label>
                       <div class="col-md-9">
                         <div class="form-group">
@@ -203,22 +184,22 @@
                         </div>
                         <a href="#" class="add-new-field">+ Add Portfolio</a>
                       </div>
-                    </div>
-                    <div id="cv" class="row form-group">
+                    </div> -->
+                    <!-- <div id="cv" class="row form-group">
                       <label class="col-md-3 col-form-label">Upload CV</label>
                       <div class="col-md-9">
                         <div class="add-cv">
                           <input type="file">CV File<i data-feather="upload-cloud"></i>
                         </div>
                       </div>
-                    </div>
-                    <div id="cover" class="row">
+                    </div> -->
+                    <!-- <div id="cover" class="row">
                       <label class="col-md-3 col-form-label">Cover Letter</label>
                       <div class="col-md-9">
                         <textarea class="tinymce-editor-1" placeholder="Description text here"></textarea>
                       </div>
-                    </div>
-                    <div id="profile" class="row">
+                    </div> -->
+                    <!-- <div id="profile" class="row">
                       <label class="col-md-3 col-form-label">Social Profile</label>
                       <div class="col-md-9">
                         <div class="form-group">
@@ -239,8 +220,8 @@
                         </div>
                         <a href="#" class="add-new-field">+ Add Social</a>
                       </div>
-                    </div>
-                    <div id="details" class="row">
+                    </div> -->
+                    <!-- <div id="details" class="row">
                       <label class="col-md-3 col-form-label">Personal Details</label>
                       <div class="col-md-9">
                         <div class="row">
@@ -286,8 +267,8 @@
                           </div>
                         </div>
                       </div>
-                    </div>
-                    <div class="row">
+                    </div> -->
+                    <!-- <div class="row">
                       <div class="col-md-9 offset-md-3">
                         <div class="form-group mt-0 terms">
                           <input class="custom-radio" type="checkbox" id="radio-4" name="termsandcondition">
@@ -296,13 +277,12 @@
                           </label>
                         </div>
                       </div>
-                    </div>
-                    <div class="form-group row">
+                    </div> -->
+                    <!-- <div class="form-group row">
                       <label class="col-md-3 col-form-label"></label>
                       <div class="col-md-9">
                         <button class="button">Publish Resume</button>
                       </div>
-                    </div>
+                    </div> -->
                   </div>
                 </form>
-              </div>
