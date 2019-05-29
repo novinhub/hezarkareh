15db3698b6755fe5063f09bc9286634b9073bb73
sajad2176 <42096679+sajad2176@users.noreply.github.com>
2019-05-29 15:56:45 +0430

    signup

--- application/views/signup/applicant.php
+++ application/views/signup/applicant.php
@@ -4,10 +4,10 @@
 			<img  src="<?php echo base_url('files/');?>images/bg/login.jpg">
 		</div>
 		<div class="col-sm-12 col-lg-3">
-			<form class="box-login" action="<?php echo base_url('login/applicant');?>" method="post">
+			<form class="box-login" action="<?php echo base_url('signup/applicant');?>" method="post">
 				<h3>ثبت نام کارجو</h3>
 				<div class="">
-                    <input type="text" name="" placeholder="شماره تماس " required>
+				<input type="text" title='شماره تلفن معتبر وارد کنید' data-toggle="tooltip" data-placement="bottom" name="tel" maxlength="11" pattern="[0-9]{11}" placeholder="شماره همراه" class="form-control" required>
 					<input type="text" name="username" placeholder="نام کاربری " required>
 					<input type="password" name="password" placeholder="رمز عبور" required>
 					<input type="submit" name="sub" value="ثبت نام">
